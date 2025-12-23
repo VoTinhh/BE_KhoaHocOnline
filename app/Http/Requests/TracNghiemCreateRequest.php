1d4b1592382be97ec87f495b6e1e276b566be129
Vo Hung Tinh <h.tinh911@tinh911gmail.com>
2025-12-23 17:54:05 +0700

    request trac nghiem create

--- app/Http/Requests/TracNghiemCreateRequest.php
+++ app/Http/Requests/TracNghiemCreateRequest.php
@@ -6,19 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TracNghiemCreateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
@@ -31,6 +23,7 @@ class TracNghiemCreateRequest extends FormRequest
             'tinh_trang'    => 'required|boolean',
         ];
     }
+    
     public function messages()
     {
         return [
