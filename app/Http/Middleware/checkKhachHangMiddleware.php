046a27ba07eb1e73008ed00393831cce8168196d
Vo Hung Tinh <h.tinh911@tinh911gmail.com>
2025-12-23 17:10:18 +0700

    check quyen khach hang

--- app/Http/Middleware/checkKhachHangMiddleware.php
+++ app/Http/Middleware/checkKhachHangMiddleware.php
@@ -15,6 +15,7 @@ class checkKhachHangMiddleware
         $user = Auth::guard('sanctum')->user();
         if($user && $user instanceof \App\Models\KhachHang )  {
 
+            
         return $next($request);
         } else {
             return response()->json([
