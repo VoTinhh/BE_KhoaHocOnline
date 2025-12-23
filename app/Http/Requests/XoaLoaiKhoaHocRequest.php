0e710cb9c12cebde5809cf0782a4270f317e1afe
Vo Hung Tinh <h.tinh911@tinh911gmail.com>
2025-12-23 21:21:46 +0700

    request loai khoa hoc delete

--- app/Http/Requests/XoaLoaiKhoaHocRequest.php
+++ app/Http/Requests/XoaLoaiKhoaHocRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class XoaLoaiKhoaHocRequest extends FormRequest
 {
-
     public function authorize(): bool
     {
         return true;
@@ -15,14 +14,15 @@ class XoaLoaiKhoaHocRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'id'        =>'required|exists:loai_khoa_hocs,id'
+            'id'        => 'required|exists:loai_khoa_hocs,id'
         ];
     }
+
     public function messages()
     {
         return  [
-            'id.required'        =>'Không tìm thấy loại khóa học',
-            'id.exists'          =>'Khóa học không tồn tại!'
+            'id.required'        => 'Không tìm thấy loại khóa học',
+            'id.exists'          => 'Khóa học không tồn tại!'
         ];
     }
 }
