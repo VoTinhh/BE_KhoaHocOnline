4305c48139e51eb24d3f9ea6ed43362442cde80a
Vo Hung Tinh <h.tinh911@tinh911gmail.com>
2025-12-23 17:54:52 +0700

    request trac nghiem delete

--- app/Http/Requests/TracNghiemDeleteRequest.php
+++ app/Http/Requests/TracNghiemDeleteRequest.php
@@ -6,25 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TracNghiemDeleteRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'id'        =>'required|exists:trac_nghiems,id'
         ];
     }
+    
     public function messages()
     {
         return [
