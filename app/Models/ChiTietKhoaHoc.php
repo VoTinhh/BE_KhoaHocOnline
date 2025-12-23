cb965defea06d8195a99232815955d700fe61120
Vo Hung Tinh <h.tinh911@tinh911gmail.com>
2025-12-23 21:27:04 +0700

    chi tiet khoa hoc model

--- app/Models/ChiTietKhoaHoc.php
+++ app/Models/ChiTietKhoaHoc.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ChiTietKhoaHoc extends Model
 {
     protected $table = 'chi_tiet_khoa_hocs';
+    
     protected $fillable = [
         'id_khoa_hoc',
         'id_khach_hang',
